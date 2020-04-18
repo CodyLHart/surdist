03388b5c2c3515f8f45ef1717cf77957e2a25315
Cody Hart <codyhart@Codys-MacBook-Pro-5.local>
2020-04-17 18:41:11 -0600

    PRoduct card styling

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -14,11 +14,17 @@ const ProductCard = (props) => {
         <div className={clName} onClick={(e) => {
             e.stopPropagation();
         }}>
-            <h1>{props.viewing ? props.viewing.displayName : null}</h1>
-            <h1>{props.viewing ? props.viewing.price : null}</h1>
-            <h1>{props.viewing ? props.viewing.material : null}</h1>
-            <h1>{props.viewing ? props.viewing.cut : null}</h1>
-            <img src={`${props.viewing ? props.viewing.photo : null}`} alt="Stuff"/>
+            <div className={styles.name}>
+                <h1 className={styles.shirtName}>{props.viewing ? props.viewing.displayName.toUpperCase() : null}</h1>          
+            </div>
+            <div className={styles.infoSection}>
+                <img className={styles.infoImg} src={`${props.viewing ? props.viewing.photo : null}`} alt={props.viewing ? props.viewing.displayName : null}/>
+                {/* <div className={styles.infoTextSection}>
+                    <h1 className={styles.infoText}>{props.viewing ? props.viewing.material.toUpperCase() : null}</h1>
+                    <h1 className={styles.infoText}>{props.viewing ? props.viewing.cut.toUpperCase() : null} CUT</h1>
+                    <h1 className={styles.infoPrice}>${props.viewing ? props.viewing.price : null}.00</h1>
+                </div> */}
+            </div>
         </div>
     )
 } 
