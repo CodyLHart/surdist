c75c7cf91fe8b13e7001b374e661486174dcad6a
Cody Hart <codyhart@Codys-MacBook-Pro-5.local>
2020-04-19 17:24:20 -0600

    Set up coming soon on cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,7 +7,12 @@ const Cart = (props) => {
         className="cart"
         id={props.cartVisible ? "" : "hidden"}
         >
-            
+            {/* <h1>TESTING</h1> */}
+            <h3>SORRY!</h3>
+            <h4>OUR WEBSITE IS UNDERGOING CONSTRUCTION</h4>
+            <p>IF YOU WOULD LIKE TO PURCHASE A SHIRT, EMAIL US AT</p>
+            <a href="mailto:surdistdesigns@gmail.com" className="cart-email">SURDISTDESIGNS@GMAIL.COM</a>
+            <p>AND WE WILL GIVE YOU A 15% DISCOUNT ON YOUR PURCHASE!</p>
         </div>
     );
 }
